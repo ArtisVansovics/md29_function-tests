676843c560c978e527e619fa0c416f4cb44bad41
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-22 00:13:32 +0300

    Defined error for isPerfectSquare(), if negative number is entered.

--- src/tests/isPerfectSquare/isPerfectSquare.ts
+++ src/tests/isPerfectSquare/isPerfectSquare.ts
@@ -1,4 +1,7 @@
-const isPerfectSquare = (num: number): boolean => {
+const isPerfectSquare = (num: number): boolean | string => {
+  if (num < 0) {
+    throw new Error('A negative number cannot be a perfect square');
+  }
   const sqrt = num ** 0.5;
 
   return sqrt % 1 === 0;
