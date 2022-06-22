6c46fd6aeecc3a2ee9e00fa79716472d2ff70858
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-22 17:29:47 +0300

    Created tests for numberOfSteps()

--- src/tests/numberOfSteps/numberOfSteps.test.ts
+++ src/tests/numberOfSteps/numberOfSteps.test.ts
@@ -1,7 +1,37 @@
 import numberOfSteps from './numberOfSteps';
 
+test('if it is defined', () => {
+  const response = numberOfSteps(2);
+
+  expect(response).toBeDefined();
+});
+
+test('if it returns a truthy value', () => {
+  const response = numberOfSteps(2);
+
+  expect(response).toBeTruthy();
+});
+
 test('if it returns steps required to reduce number to zero', () => {
   const response = numberOfSteps(14);
 
   expect(response).toEqual(6);
+});
+
+test('if it can handle large numbers', () => {
+  const response = numberOfSteps(9001);
+
+  expect(response).toEqual(19);
+});
+
+test('if it returns zero when zero is given as a parameter', () => {
+  const response = numberOfSteps(0);
+
+  expect(response).toEqual(0);
+});
+
+test('if it can accept negative numbers', () => {
+  const response = numberOfSteps(-8);
+
+  expect(response).toEqual(4);
 });
