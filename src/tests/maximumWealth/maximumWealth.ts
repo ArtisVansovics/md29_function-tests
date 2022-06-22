3d10792f3f3dcde45ab1290f808e38e44a2301c1
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-22 14:21:47 +0300

    maximumWealth() now returns zero, when it is given an array of empty arrays

--- src/tests/maximumWealth/maximumWealth.ts
+++ src/tests/maximumWealth/maximumWealth.ts
@@ -1,11 +1,13 @@
 const maximumWealth = (accounts: number[][]): number => {
   const arr: number[] = [];
 
-  accounts.forEach((account) => (
-    arr.push(account.reduce((a, b) => a + b))
-  ));
+  if (accounts.flat().length) {
+    accounts.forEach((account) => (
+      arr.push(account.reduce((a, b) => a + b))
+    ));
 
-  return Math.max(...arr);
+    return Math.max(...arr);
+  } return 0;
 };
 
 export default maximumWealth;
